9e680e1708c286e169e944c05fd73bd9a46216ea
Kacper Duda <kapieksperimental@gmail.com>
2024-12-02 23:54:32 +0100

    dodano styl do admin panel

--- page/adminpanel.php
+++ page/adminpanel.php
@@ -3,12 +3,35 @@ include './include/check.php'; // Sprawdzenie, czy użytkownik jest zalogowany i
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style/style.css">
     <title>Panel Administracyjny</title>
 </head>
+
 <body>
+    <div class="baner">
+        <h1 style="text-align: center;">Panel Administracyjny</h1>
+        <hr>
+    </div>
+
+    <div class="adminpanel_glowny">
+        <h2>Co Chesz Zrobic?</h2>
+
+        <br><br>
+        <button onclick="sendToPage('adminpanel_createnew.php')">Stworz Nową Rozgrywkę</button>
+        <button onclick="sendToPage('adminpanel_modify.php')">Zaakutalizuj Rozgrywkę</button>
+    </div>
     
+    <script>
+
+        function sendToPage(location){
+            window.location.assign(location);
+        }
+    </script>
+
 </body>
+
 </html>
